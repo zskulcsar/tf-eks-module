c82607ce49dc441a1841e296ab60406cd6842b11
Zsolt Kulcsar <zskulcsar@gmail.com>
2018-06-20 10:49:30 +0100

    More verbose description

--- variables.tf
+++ variables.tf
@@ -7,33 +7,38 @@ variable "aws_region" {
 
 # Variables where no default can be provided
 variable "cluster_name" {
-  description = "The name of the cluster"
+  description = "The name of the cluster. Please use alphanum '-' 
+                 and '_' only as this is used as part of the name for many resources"
 }
 
 variable "vpc_id" {
-  description = "The id of the VPC where the cluster should be created"
+  description = "The id of the VPC where the cluster should be created."
 }
 
 variable "cp_subnets" {
   type = "list"
-  description = "The subnets where the control plane should live. At least 2 separate subnets"
+  description = "The subnets where the control plane should live. At least 2 separate subnets. 
+                 Ideally control_plane goes to a public subnet."
 }
 
 variable "worker_subnets" {
   type = "list"
+  description = "The subnets where the worker nodes should live. No restrictions on the number of subnets. 
+                 Ideally the worker nodes are living in a private subnet."
 }
 
 variable "ssh_key" {
-  
+  description = "The ssh key used to login to the worker nodes. Normally you don't need this, but you'll never know."
 }
 
 variable "sg_bastion" {
-
+ description = "The security group for the bastion instance. It is assumed that the worker nodes
+                can't be accessed via ssh other than using the bastion."
 }
 
 # Variables with sensible defaults
 variable "kube_version" {
-  description = "The kubernetes version to be used"
+  description = "The kubernetes version to be used."
   default = "1.10"
 }
 
@@ -48,6 +53,7 @@ variable "worker_type" {
 
 variable "pods_per_node" {
   type = "map"
+  description = "The number of pods a worker node can have as a maximum. Defined by AWS"
 
   default = {
     c4.large    = 29
@@ -110,7 +116,7 @@ variable "pods_per_node" {
 
 variable "os_name" {
   default = "amazon"
-  description = "The name of the OS, currently 'ubuntu' and 'amazon' is supported"
+  description = "The name of the OS, currently only 'amazon' is supported."
 }
 
 variable "worker_ami" {
@@ -121,7 +127,7 @@ variable "worker_ami" {
     us-west-2.amazon = "ami-73a6e20b"
     us-east-1.amazon = "ami-dea4d5a1"
     # Canonical
-    us-west-2.ubuntu = "ami-7183c009"
-    us-east-1.ubuntu = "ami-75692f0a"
+    # us-west-2.ubuntu = "ami-7183c009"
+    # us-east-1.ubuntu = "ami-75692f0a"
   }
 }
