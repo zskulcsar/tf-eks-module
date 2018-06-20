0ac8bfc5b1ba7f62f3f6d8f0411943fcc22ad7e1
Zsolt Kulcsar <zskulcsar@gmail.com>
2018-06-20 10:13:56 +0100

    kubeconfig, configmap, userdata are now templates

--- kubeconfig.tf
+++ kubeconfig.tf
@@ -1,65 +1,29 @@
 # kubectl config
-locals {
-  kubeconfig = <<KUBECONFIG
+data "template_file" "kubeconfig" {
+  template = "${file("${path.module}/templates/kubeconfig.yaml")}"
 
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${aws_eks_cluster.default.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.default.certificate_authority.0.data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: heptio-authenticator-aws
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster_name}"
-KUBECONFIG
+  vars {
+    cluster_name                = "${var.cluster_name}"
+    cluster_endpoint            = "${aws_eks_cluster.default.endpoint}"
+    certificate_authority_data  = "${aws_eks_cluster.default.certificate_authority.0.data}"
+  }
 }
 
 resource "local_file" "kubeconfig" {
-    content     = "${local.kubeconfig}"
-    filename = "./kubeconfig-${var.cluster_name}.yaml"
+    content     = "${data.template_file.kubeconfig.rendered}"
+    filename    = "./${var.cluster_name}/kubeconfig.yaml"
 }
 
 # Worker node config for kube
-locals {
-  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+data "template_file" "worker_auth_config" {
+  template = "${file("${path.module}/templates/aws-worker-configmap.yaml")}"
 
-
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: aws-auth
-  namespace: kube-system
-data:
-  mapRoles: |
-    - rolearn: ${aws_iam_role.eks_worker.arn}
-      username: system:node:{{EC2PrivateDNSName}}
-      groups:
-        - system:bootstrappers
-        - system:nodes
-CONFIGMAPAWSAUTH
+  vars {
+    worker_role_arn = "${aws_iam_role.eks_worker.arn}"
+  }
 }
 
-output "config_map_aws_auth" {
-  value = "${local.config_map_aws_auth}"
-}
-
-resource "local_file" "worker_config_map" {
-    content     = "${local.config_map_aws_auth}"
-    filename = "./configmap-worker-${var.cluster_name}.yaml"
+resource "local_file" "worker_auth_config" {
+    content     = "${data.template_file.worker_auth_config.rendered}"
+    filename    = "./${var.cluster_name}/worker-aws-auth.yaml"
 }
